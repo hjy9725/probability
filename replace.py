251d9a30eca9b3778fcfa9c4226d2ab89439dc17
hjy9725 <huang19970205@qq.com>
2024-11-16 21:35:50 +0800

    test

--- replace.py
+++ replace.py
@@ -38,7 +38,9 @@ def replace_dollar_signs_in_md_files(root_dir="."):
                 new_content = new_content.replace("$ $", "$$")
                 new_content = new_content.replace("① $", "①$")
                 new_content = new_content.replace(" $ ", "$ ")
-                
+                new_content = new_content.replace("\{", "\lbrace ")
+                new_content = new_content.replace("\}", " \rbrace")
+
                 # 写回文件
                 with open(file_path, "w", encoding="utf-8") as f:
                     f.write(new_content)
