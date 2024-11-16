5c496616723a86be6f0ec6bb26e0316e3ace3a5b
hjy9725 <huang19970205@qq.com>
2024-11-16 20:55:28 +0800

    in

--- replace.py
+++ replace.py
@@ -37,6 +37,7 @@ def replace_dollar_signs_in_md_files(root_dir="."):
                 new_content = new_content.replace("$", " $")
                 new_content = new_content.replace("$ $", "$$")
                 new_content = new_content.replace("① $", "①$")
+                new_content = new_content.replace(" $ ", "$ ")
                 
                 # 写回文件
                 with open(file_path, "w", encoding="utf-8") as f:
