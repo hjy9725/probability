0ecfd24e1898e1e362b636badc1806a29739243c
hjy9725 <huang19970205@qq.com>
2024-11-17 21:36:19 +0800

    give up

--- replace.py
+++ replace.py
@@ -113,6 +113,7 @@ def replace_dollar_signs_in_md_files(root_dir="."):
                 new_content = new_content.replace("\cup ", "\cup")
                 new_content = new_content.replace("\cup ", "\cup")
                 new_content = new_content.replace("\cup", "\cup ")
+                new_content = new_content.replace("** $", "**$")
 
                 # 写回文件
                 with open(file_path, "w", encoding="utf-8") as f:
